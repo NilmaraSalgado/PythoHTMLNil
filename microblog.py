095b2862b01a61acfc13faedaa57b297c914861d
NilmaraSalgado <nilmarasalgado@gmail.com>
2021-03-18 18:06:15 +0000

    Hello World

---
+++ microblog.py
@@ -0,0 +1,12 @@
+from flask import Flask 
+
+app = Flask("microblog") #programa que fica ligado na internet e recebe microrequesições e vai ter rotas  (liga url com funçao do pyton)
+
+#quero linkar essa função com uma rota (docom 2 urls)
+#toda vez que a pessoa entrar no ocean.com e encontrar / vai retornar a função
+#vou fazer com esse decorator em cima da função 
+@app.route("/")
+def index ():
+    return "Hello World"
+#rodar o servidor que vai fazer esse link
+app.run()
